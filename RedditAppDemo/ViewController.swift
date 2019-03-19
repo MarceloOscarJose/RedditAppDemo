cc132d22d5aed8939cd81fd0f0b490b96719462a
Marcelo Oscar José <marcelo.oscar.jose@gmail.com>
2019-03-19 07:13:07 -0300

    New single project

---
+++ RedditAppDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RedditAppDemo
+//
+//  Created by Marcelo José on 19/03/2019.
+//  Copyright © 2019 Marcelo José. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
